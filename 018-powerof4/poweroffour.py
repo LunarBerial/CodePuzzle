e3a1e92f63b6722f3945d32b97f011206d27ee3c
LunarBerial <970139634@qq.com>
2020-02-11 18:55:05 +0800

    update power of 4

--- 018-powerof4/poweroffour.py
+++ 018-powerof4/poweroffour.py
@@ -1,6 +1,13 @@
 #coding: utf-8
+# leetcode 342: https://leetcode.com/problems/power-of-four/description/ 判断一个数是否为4的幂
 
+#方法1：递归除4：
+def ispoweroffour_raw(n:int):
+    while(n && n%4 == 0):
+        n = n/4
+    return n == 1
 
+#方法 2： 如果不让用递归的方式。换一种思路：那么4的幂一定是2的幂。 并且 该数 - 1一定是3的幂。
 def ispoweroffour(n:int):
 
     if n == 1:
@@ -13,7 +20,7 @@ def ispoweroffour(n:int):
         else:
             return False
 
-
+# 方法3： 转化为2进制。4的幂数一定满足有奇数个1.
 def ispoweroffour2(n:int):
 
     binnum = bin(n)[2:]
